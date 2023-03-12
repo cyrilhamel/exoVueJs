cd490f3a71ccc83c3048c2321b9352d53c4ba523
Cyril Hamel <xizor64@gmail.com>
2023-03-12 22:42:16 +0100

    majWatchFilm

---
+++ watchList/app.js
@@ -0,0 +1,18 @@
+const app = Vue.createApp({
+    data() {
+      return { 
+        nomFilm:'',
+        mesFilms:[]
+      };
+    },
+    methods: {
+     ajouterFilm(){
+       this.mesFilms.push(this.nomFilm);
+     },
+
+     supprimerFilm(unIndex){
+        this.mesFilms.splice(unIndex,1);
+      }
+    },
+  });
+  app.mount('#monApp');
